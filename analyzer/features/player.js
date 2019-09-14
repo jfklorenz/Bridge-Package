e389115d7e4f41e953c51b26fb67c2952e947c7a
Julian Lorenz <jfk.lorenz@gmail.com>
2019-09-14 18:54:40 +0200

    [NEW] folder/analyzer/ content not yet working

---
+++ analyzer/features/player.js
@@ -0,0 +1,17 @@
+const { shuffle } = require("../features/shuffle.js")
+const Hand = require("../features/hand.js");
+
+// ================================================================
+
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.hand = null;
+  }
+
+
+}
+
+
+// ================================================================
+module.exports = Player
