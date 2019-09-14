d1f63f93e1c5e2a5f4f70e19628fd2517475c9a9
Julian Lorenz <jfk.lorenz@gmail.com>
2019-09-14 22:07:10 +0200

    [REFACTOR] statistics/...

--- statistics/__tests__/missing.js
+++ statistics/__tests__/missing.js
@@ -1,5 +1,5 @@
 const expect = require("expect.js");
-const { missingCards, missingHighcards} = require("../features/missing.js");
+const { allDistributionsWithProbabilityByMissingCards, missingHighcards} = require("../features/missing.js");
 
 // ================================================================
 describe('test/missing.js - Missing Cards Distribution & Probability', function() {
@@ -7,57 +7,57 @@ describe('test/missing.js - Missing Cards Distribution & Probability', function(
   // ================================================================
   // Missing Cards
   it("1.0. Missing Cards / Input", function() {
-    expect(missingCards).withArgs("a").to.throwException();
-    expect(missingCards).withArgs("Z").to.throwException();
-    expect(missingCards).withArgs(-1).to.throwException();
-    expect(missingCards).withArgs(1.234).to.throwException();
-    expect(missingCards).withArgs(14).to.throwException();
+    expect(allDistributionsWithProbabilityByMissingCards).withArgs("a").to.throwException();
+    expect(allDistributionsWithProbabilityByMissingCards).withArgs("Z").to.throwException();
+    expect(allDistributionsWithProbabilityByMissingCards).withArgs(-1).to.throwException();
+    expect(allDistributionsWithProbabilityByMissingCards).withArgs(1.234).to.throwException();
+    expect(allDistributionsWithProbabilityByMissingCards).withArgs(14).to.throwException();
   });
 
   it("1.1. Missing Cards / Base Cases", function() {
     // Missing 1 Card
     // [0,1], 50
-    expect(missingCards(1)[0][0][0]).to.equal(0);
-    expect(missingCards(1)[0][0][1]).to.equal(1);
-    expect(missingCards(1)[0][1]).to.equal(50);
+    expect(allDistributionsWithProbabilityByMissingCards(1)[0][0][0]).to.equal(0);
+    expect(allDistributionsWithProbabilityByMissingCards(1)[0][0][1]).to.equal(1);
+    expect(allDistributionsWithProbabilityByMissingCards(1)[0][1]).to.equal(50);
     // [1,0], 50
-    expect(missingCards(1)[1][0][0]).to.equal(1);
-    expect(missingCards(1)[1][0][1]).to.equal(0);
-    expect(missingCards(1)[1][1]).to.equal(50);
+    expect(allDistributionsWithProbabilityByMissingCards(1)[1][0][0]).to.equal(1);
+    expect(allDistributionsWithProbabilityByMissingCards(1)[1][0][1]).to.equal(0);
+    expect(allDistributionsWithProbabilityByMissingCards(1)[1][1]).to.equal(50);
   });
 
   it("1.2. Missing Cards / Cases", function() {
     // Missing 2 Cards
     // [0,2], 24
-    expect(missingCards(2)[0][0][0]).to.equal(0);
-    expect(missingCards(2)[0][0][1]).to.equal(2);
-    expect(missingCards(2)[0][1]).to.equal(24);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[0][0][0]).to.equal(0);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[0][0][1]).to.equal(2);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[0][1]).to.equal(24);
     // [1,1], 52
-    expect(missingCards(2)[1][0][0]).to.equal(1);
-    expect(missingCards(2)[1][0][1]).to.equal(1);
-    expect(missingCards(2)[1][1]).to.equal(52);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[1][0][0]).to.equal(1);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[1][0][1]).to.equal(1);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[1][1]).to.equal(52);
     // [2,0], 24
-    expect(missingCards(2)[2][0][0]).to.equal(2);
-    expect(missingCards(2)[2][0][1]).to.equal(0);
-    expect(missingCards(2)[2][1]).to.equal(24);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[2][0][0]).to.equal(2);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[2][0][1]).to.equal(0);
+    expect(allDistributionsWithProbabilityByMissingCards(2)[2][1]).to.equal(24);
 
     // Missing 3 Cards
     // [0,3], 11
-    expect(missingCards(3)[0][0][0]).to.equal(0);
-    expect(missingCards(3)[0][0][1]).to.equal(3);
-    expect(missingCards(3)[0][1]).to.equal(11);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[0][0][0]).to.equal(0);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[0][0][1]).to.equal(3);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[0][1]).to.equal(11);
     // [1,2], 39
-    expect(missingCards(3)[1][0][0]).to.equal(1);
-    expect(missingCards(3)[1][0][1]).to.equal(2);
-    expect(missingCards(3)[1][1]).to.equal(39);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[1][0][0]).to.equal(1);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[1][0][1]).to.equal(2);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[1][1]).to.equal(39);
     // [2,1], 39
-    expect(missingCards(3)[2][0][0]).to.equal(2);
-    expect(missingCards(3)[2][0][1]).to.equal(1);
-    expect(missingCards(3)[2][1]).to.equal(39);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[2][0][0]).to.equal(2);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[2][0][1]).to.equal(1);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[2][1]).to.equal(39);
     // [3,0], 11
-    expect(missingCards(3)[3][0][0]).to.equal(3);
-    expect(missingCards(3)[3][0][1]).to.equal(0);
-    expect(missingCards(3)[3][1]).to.equal(11);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[3][0][0]).to.equal(3);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[3][0][1]).to.equal(0);
+    expect(allDistributionsWithProbabilityByMissingCards(3)[3][1]).to.equal(11);
   });
 
 // ================================================================
--- statistics/features/missing.js
+++ statistics/features/missing.js
@@ -2,9 +2,10 @@
 const {factorial, percentageRounded} = require("./math.js");
 
 // ================================================================
-// missingCards
-function missingCards(cardCnt) {
 
+// ================================================================
+// All Distributions with Probability by Missing Cards
+function allDistributionsWithProbabilityByMissingCards(cardCnt) {
   if (!([...Array(14).keys()].slice(1).includes(cardCnt))) throw "1014: Input invalid.";
 
   let results = [];
@@ -35,23 +36,47 @@ function missingCards(cardCnt) {
     result *= factorial(resultCurrent[0] + resultCurrent[1]) / (factorial(resultCurrent[0]) * factorial(resultCurrent[1]));
     results[j][1] = percentageRounded(result);
   }
-
   return results;
 }
 
 // ================================================================
-// missingHighcards
-function missingHighcards(cardCnt, highcardCnt) {
-  let results = [];
-  for (var i = 0; i <= cardCnt; i++) {
-    results.push([[i, cardCnt - i], 0]);
+// Probability of Missing Cards by Disribution
+function probabilityOfMissingCardsByDistribution(distribution) {
+  let results = allDistributionsWithProbabilityByMissingCards(distribution[0] + distribution[1]);
+  for (var i = 0; i < results.length; i++) {
+    if (results[i][0][0] === distribution[0] && results[i][0][1] === distribution[1]) {
+      return results[i][1];
+    }
   }
+}
 
+// ================================================================
+// missingHighcards
+function allDistributionsWithProbabilityByMissingCardsAndHighcards(cardCnt, highcardCnt) {
+  let results = [];
+
+  for (var i = 0; i <= cardCnt; i++) {
+    let result0 = []
+    for (var j = 0; j <= highcardCnt; j++) {
+      if (i >= j && cardCnt -i >= highcardCnt - j) {
+        result0.push([[i, cardCnt - i], [j, highcardCnt - j], 
+            percentageRounded(
+                probabilityOfMissingCardsByDistribution([i, cardCnt - i]) * 
+                probabilityOfMissingCardsByDistribution([j, highcardCnt - j]) / 10000)]);
+      }
+    }
+    results.push(result0);
+  }
   return results
+}
+
+let test = allDistributionsWithProbabilityByMissingCardsAndHighcards(4, 2);
+for (var i = 0; i < test.length; i++) {
+  console.log(test[i])
 }
 
 // ================================================================
 module.exports = {
-  missingCards,
-  missingHighcards
+  allDistributionsWithProbabilityByMissingCards,
+  allDistributionsWithProbabilityByMissingCardsAndHighcards
 }
