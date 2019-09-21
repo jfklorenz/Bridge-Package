f435b1444c812a0c3a1cb8af75f2f83309b80946
Julian Lorenz <jfk.lorenz@gmail.com>
2019-09-21 14:31:59 +0200

    [FIX] index.js

--- statistics/index.js
+++ statistics/index.js
@@ -2,7 +2,7 @@ module.exports = {
     binomial : require("./features/math.js"),
     factorial : require("./features/math.js"),
     percentageRounded : require("./features/math.js"),
-    hcpRange : require("./features/hcp.js"),
+    hcp : require("./features/hcp.js"),
     distribution : require("./features/distribution.js"),
     allDistributionsWithProbabilityByMissingCards : require("./features/missing.js"),
     probabilityOfMissingCardsByDistribution : require("./features/missing.js"),
